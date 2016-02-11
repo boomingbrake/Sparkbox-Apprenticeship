a6eb067a28fe497a40b4ac38a130d5c801d64f7a
boomingbrake <brittanywebbe@gmail.com>
2016-02-11 11:50:01 -0500

    Add rewrite of bowling kata - no frame class

---
+++ Ruby/TDD/bowling2/lib/bowling.rb
@@ -0,0 +1,31 @@
+class Bowling
+  attr_accessor :rolls
+
+  def initialize
+    @rolls = Array.new
+  end
+
+  def sum_of(frame)
+    sum = frame.collect { |roll| roll.nil? ? 0 : roll }.reduce(:+)
+  end
+
+  def score_game
+    score = 0
+    i = 1
+    while !rolls.empty? && i <= 10 do
+      frame = []
+      frame << rolls.shift
+      frame << rolls.shift if frame[0] != 10
+
+      if sum_of(frame) == 10 #add bonus
+        frame << rolls.first
+        frame << rolls[1] if frame.first == 10 #strike extra roll
+      end
+
+      score += sum_of(frame)
+      i+=1
+    end
+    score
+  end
+
+end
---
+++ Ruby/TDD/bowling2/spec/bowling_spec.rb
@@ -0,0 +1,40 @@
+require_relative "spec_helper"
+require_relative "../lib/bowling"
+
+RSpec.describe Bowling do
+
+  context 'when the game starts'
+  it 'has a score of 0' do
+    game = Bowling.new
+    expect(game.score_game).to eq(0)
+  end
+
+  context 'after first roll'
+  it 'has stored knocked down pins' do
+    game = Bowling.new
+    game.rolls << 2
+    expect(game.rolls).to eq([2])
+  end
+  
+  context 'after open frames'
+  it 'has a score of 25' do
+    game = Bowling.new
+    game.rolls << 2 << 5 << 6 << 3 << 7 <<2
+    expect(game.score_game).to eq(25)
+  end
+
+  context 'after game of all 5s'
+  it 'has a score of 150' do
+    game = Bowling.new
+    21.times { game.rolls << 5 }
+    expect(game.score_game).to eq(150)
+  end
+
+  context 'after game of all strikes'
+  it 'has a score of 300' do
+    game = Bowling.new
+    12.times { game.rolls << 10}
+    expect(game.score_game).to eq(300)
+  end
+
+end
