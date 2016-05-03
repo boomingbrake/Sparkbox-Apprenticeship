698e3a2bbe401675b6232b20d0d9b4fc8c27152f
boomingbrake <brittanywebbe@gmail.com>
2016-05-03 15:23:45 -0400

    Pattern Library up and running as well as first Ruby On Rails app

--- RoR/Zombies/app/controllers/tweets_controller.rb
+++
@@ -1,9 +0,0 @@
-class TweetsController < ApplicationController
-  def show
-    @tweet = Tweet.find(params[:id])
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @tweet}
-    end
-  end
-end
--- RoR/Zombies/app/models/tweet.rb
+++
@@ -1,4 +0,0 @@
-class Tweet < ActiveRecord::Base
-has_many :tweets
-belongs_to :zombue
-end
