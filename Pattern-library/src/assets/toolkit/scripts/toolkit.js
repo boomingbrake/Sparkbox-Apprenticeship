6271b021174bd174cf1c50226da83fdbb649855f
boomingbrake <brittanywebbe@gmail.com>
2016-05-03 15:26:16 -0400

    Pattern Library up and running as well as first Ruby On Rails app

---
+++ Pattern-library/src/assets/toolkit/scripts/toolkit.js
@@ -0,0 +1,28 @@
+/**
+ * Toolkit JavaScript
+ */
+
+'use strict';
+var $ = require('jQuery');
+
+$(document).ready(function() {
+  $("#tabTop").click(function() {
+    if ($(this).hasClass("is-opened")) {
+      $(this).addClass("is-closed").removeClass("is-opened")
+      $("#tabElementTop").addClass("is-closed").removeClass("is-opened")
+    } else {
+      $(this).removeClass("is-closed").addClass("is-opened")
+      $("#tabElementTop").removeClass("is-closed").addClass("is-opened")
+    }
+  });
+  $("#tabRight").click(function() {
+    if ($(this).hasClass("is-opened")) {
+      $(this).addClass("is-closed").removeClass("is-opened")
+        $("#tabElementRight").addClass("is-closed").removeClass("is-opened")
+    } else {
+      $(this).removeClass("is-closed").addClass("is-opened")
+      $("#tabElementRight").removeClass("is-closed").addClass("is-opened")
+    }
+  });
+
+});
